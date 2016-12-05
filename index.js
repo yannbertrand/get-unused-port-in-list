7e815db12a8a3848afcc988cb0f52cc2f2171953
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2016-12-05 21:09:31 +0100

    do not use a closure

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ module.exports = (ports) => {
   return new Promise((resolve, reject) => {
 
     let numberOfPorts = ports.length;
-    for (_port of ports) {
-      ((port) => {
-        isPortBusy(port)
-          .then(() => {
-            numberOfPorts--;
-            
-            if (numberOfPorts === 0) {
-              return reject('All ports in the list are used');
-            }
-          })
-          .catch(() => {
-            return resolve(port);
-          });
-      })(_port);
+    for (const port of ports) {
+      isPortBusy(port)
+        .then(() => {
+          numberOfPorts--;
+
+          if (numberOfPorts === 0) {
+            return reject('All ports in the list are used');
+          }
+        })
+        .catch(() => {
+          return resolve(port);
+        });
     }
 
   });
